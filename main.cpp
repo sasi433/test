29437cb7e6c4cb60ce78849a42fd5d22eb21d840
Sasanka Maddala <sasiprofessional13@gmail.com>
2018-08-20 11:28:28 +0530

    Start

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+#include <conio.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int a,b,c;
+    std::cout<<"Enter a value";
+    std::cin>>a;
+    std::cout<<"Enter b value";
+    std::cin>>b;
+    c=a+b;
+    std::cout<<"Sum of a+b is: "<< c;
+
+    return 0;
+}
